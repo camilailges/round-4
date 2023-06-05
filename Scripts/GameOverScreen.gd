bd0e5eda290f8c5b43bef74e7ac6d32659e419fe
Eduardo Menegaz Pretto <eduardo.pretto@edu.pucrs.br>
2023-06-05 10:28:17 -0300

    criadas telas de game over e win

---
+++ Scripts/GameOverScreen.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_Play_Again_pressed() -> void:
+	get_tree().change_scene("res://scenes/Level1.tscn")
+	
+
+
+func _on_Quit_pressed() -> void:
+	get_tree().quit()
