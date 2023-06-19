cd680809303bec634bd522e4d67bfc87bd6c3c1d
jpfeijo <j.feijo@edu.pucrs.br>
2023-06-19 16:46:59 -0300

    barra de ritmo funcionando

--- scenes/KinematicBody2D.gd
+++ scenes/KinematicBody2D.gd
@@ -1,16 +1,23 @@
 extends KinematicBody2D
 
-var speed = 100
 var arrow: Sprite
-var count = 0
 var barBackground: Sprite
+var greenSpot: Sprite
+var speed = 250
+var new_speed = 0
+
+var cliques_restantes = 10
+var total_de_acertos = 0
+
+var clicou = true
 
 func _ready():
 	arrow = $Arrow
 	barBackground = $BarBackground
+	greenSpot = $GreenSpot
 
 func _physics_process(delta):
-	var motion = Vector2(speed * delta, 0)  # Positive speed for right to left movement
+	var motion = Vector2(speed * delta, 0)
 	arrow.position += motion
 
 	var arrowHalfWidth = arrow.texture.get_width() * arrow.scale.x * 0.5
@@ -20,14 +27,31 @@ func _physics_process(delta):
 	var leftBoundary = barBackground.position.x - barHalfWidth + arrowHalfWidth
 
 	if arrow.position.x > rightBoundary:
-		speed = -abs(speed)
+		speed = -abs(rand_range(200, 600))
+		clicou = true
+		
 	elif arrow.position.x < leftBoundary:
-		speed = abs(speed)
+		speed = abs(rand_range(200, 600))
+		clicou = true
 
-	var spot_position = $GreenSpot.position.x
-	if abs(spot_position) < 10:
-		if Input.is_action_just_pressed("ui_accept"):
-			if abs(arrow.position.x - barBackground.position.x) <= barHalfWidth - arrowHalfWidth:
-				count += 1
-				print("Count: ", count)
-			# Add your logic here for when the player successfully hits the spot
+	var spotPosition = $GreenSpot.position.x
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		if clicou:
+			if arrow.position.x > greenSpot.position.x - 20 and arrow.position.x < greenSpot.position.x + 20:
+				total_de_acertos += 1
+				cliques_restantes -= 1				
+				clicou = false
+				print("Acertou: ", total_de_acertos)
+				print("Cliques restantes: ", cliques_restantes)
+			else:
+				cliques_restantes -= 1
+				clicou = false				
+				print("Errou")
+				print("Cliques restantes: ", cliques_restantes)
+	
+	if cliques_restantes + total_de_acertos < 5:
+		get_tree().change_scene("res://scenes/GameOverScreen.tscn")
+		
+	if total_de_acertos >= 5:
+		get_tree().change_scene("res://scenes/WinScreen.tscn")
