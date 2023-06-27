f5e9329e3c23d8d5f452428d2190598c387b75fb
Ilges <camila.ilges@sap.com>
2023-06-27 17:45:05 -0300

    level 2 mainly working

--- scenes/KinematicBody2D.gd
+++ scenes/KinematicBody2D.gd
@@ -11,10 +11,33 @@ var total_de_acertos = 0
 
 var clicou = true
 
+var bot1 : KinematicBody2D
+var bot2: KinematicBody2D
+var bot3: KinematicBody2D
+var bot4: KinematicBody2D
+var bot5: KinematicBody2D
+var bot6: KinematicBody2D
+var corda: KinematicBody2D
+var bots := []
+
 func _ready():
 	arrow = $Arrow
 	barBackground = $BarBackground
 	greenSpot = $GreenSpot
+	bot1 = get_node("../BotLevel2")
+	bot2 = get_node("../Bot2Level2")
+	bot3 = get_node("../Bot3Level2")
+	bot4 = get_node("../Bot4Level2")
+	bot5 = get_node("../Bot5Level2")
+	bot6 = get_node("../Bot6Level2")
+	corda = get_node("../Corda")
+	
+	bots.append(bot1)
+	bots.append(bot2)
+	bots.append(bot3)
+	bots.append(bot4)
+	bots.append(bot5)
+	bots.append(bot6)
 
 func _physics_process(delta):
 	var motion = Vector2(speed * delta, 0)
@@ -27,14 +50,14 @@ func _physics_process(delta):
 	var leftBoundary = barBackground.position.x - barHalfWidth + arrowHalfWidth
 
 	if arrow.position.x > rightBoundary:
-		speed = -abs(rand_range(200, 600))
+		speed = -abs(rand_range(200, 200))
 		if clicou:
 			cliques_restantes -= 1
 			print("Cliques restantes: ", cliques_restantes)
 		clicou = true
 		
 	elif arrow.position.x < leftBoundary:
-		speed = abs(rand_range(200, 600))
+		speed = abs(rand_range(200, 200))
 		if clicou:
 			cliques_restantes -= 1
 			print("Cliques restantes: ", cliques_restantes)
@@ -48,11 +71,15 @@ func _physics_process(delta):
 				total_de_acertos += 1
 				cliques_restantes -= 1				
 				clicou = false
+				bot1.animation.play("avanca")
+				corda.position.x -= 55
 				print("Acertou: ", total_de_acertos)
 				print("Cliques restantes: ", cliques_restantes)
 			else:
 				cliques_restantes -= 1
-				clicou = false				
+				clicou = false	
+				bot1.animation.play("tras")
+				corda.position.x += 55		
 				print("Errou")
 				print("Cliques restantes: ", cliques_restantes)
 	
