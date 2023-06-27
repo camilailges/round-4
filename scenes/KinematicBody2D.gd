a1818469548c39f5d737b2c8b2d984d68f14b249
Ilges <camila.ilges@sap.com>
2023-06-27 18:46:41 -0300

    all bots working

--- scenes/KinematicBody2D.gd
+++ scenes/KinematicBody2D.gd
@@ -20,6 +20,9 @@ var bot6: KinematicBody2D
 var corda: KinematicBody2D
 var bots := []
 
+var delay_timer = 0
+var delay_duration = 2
+
 func _ready():
 	arrow = $Arrow
 	barBackground = $BarBackground
@@ -31,13 +34,13 @@ func _ready():
 	bot5 = get_node("../Bot5Level2")
 	bot6 = get_node("../Bot6Level2")
 	corda = get_node("../Corda")
-	
-	bots.append(bot1)
-	bots.append(bot2)
-	bots.append(bot3)
-	bots.append(bot4)
-	bots.append(bot5)
-	bots.append(bot6)
+	bots = [bot1, bot2, bot3, bot4, bot5, bot6]
+#	bots.append(bot1)
+#	bots.append(bot2)
+#	bots.append(bot3)
+#	bots.append(bot4)
+#	bots.append(bot5)
+#	bots.append(bot6)
 
 func _physics_process(delta):
 	var motion = Vector2(speed * delta, 0)
@@ -71,20 +74,41 @@ func _physics_process(delta):
 				total_de_acertos += 1
 				cliques_restantes -= 1				
 				clicou = false
-				bot1.animation.play("avanca")
-				corda.position.x -= 55
+				for bot in bots:
+					bot.animation.play("avanca")
+				corda.position.x -= 15
 				print("Acertou: ", total_de_acertos)
 				print("Cliques restantes: ", cliques_restantes)
 			else:
 				cliques_restantes -= 1
-				clicou = false	
-				bot1.animation.play("tras")
-				corda.position.x += 55		
+				clicou = false
+				for bot in bots:
+					bot.animation.play("tras")
+				corda.position.x += 15		
 				print("Errou")
 				print("Cliques restantes: ", cliques_restantes)
 	
 	if cliques_restantes + total_de_acertos < 5:
-		get_tree().change_scene("res://scenes/GameOverScreen.tscn")
+		corda.position.x += 200
+		if delay_timer < delay_duration:
+			delay_timer += delta
+		else:
+			get_tree().change_scene("res://scenes/GameOverScreen.tscn")
+			# Realize a operação que deseja executar após o atraso
+			# ...
+			# Redefina o temporizador de atraso
+			delay_timer = 0
+		
+		
 		
 	if total_de_acertos >= 5:
-		get_tree().change_scene("res://scenes/WinScreen.tscn")
+		corda.position.x -= 200
+		if delay_timer < delay_duration:
+			delay_timer += delta
+		else:
+			get_tree().change_scene("res://scenes/WinScreen.tscn")
+			# Realize a operação que deseja executar após o atraso
+			# ...
+			# Redefina o temporizador de atraso
+			delay_timer = 0
+		
