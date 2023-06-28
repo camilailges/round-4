98fd391304defc1ab49e3e2bd104a538bf48fc03
Ilges <camila.ilges@sap.com>
2023-06-28 18:43:29 -0300

    action players slowly falling and completely fall

--- scenes/KinematicBody2D.gd
+++ scenes/KinematicBody2D.gd
@@ -11,6 +11,7 @@ var total_de_acertos = 0
 
 var clicou = true
 
+var arrowPlayer : Node2D
 var bot1 : KinematicBody2D
 var bot2: KinematicBody2D
 var bot3: KinematicBody2D
@@ -19,9 +20,10 @@ var bot5: KinematicBody2D
 var bot6: KinematicBody2D
 var corda: KinematicBody2D
 var bots := []
+var arrows := []
 
 var delay_timer = 0
-var delay_duration = 2
+var delay_duration = 2.5
 
 func _ready():
 	arrow = $Arrow
@@ -34,13 +36,15 @@ func _ready():
 	bot5 = get_node("../Bot5Level2")
 	bot6 = get_node("../Bot6Level2")
 	corda = get_node("../Corda")
+	arrowPlayer = get_node("../ArrowPlayer")
 	bots = [bot1, bot2, bot3, bot4, bot5, bot6]
-#	bots.append(bot1)
+	arrows.append(arrowPlayer)
 #	bots.append(bot2)
 #	bots.append(bot3)
 #	bots.append(bot4)
 #	bots.append(bot5)
 #	bots.append(bot6)
+	print(arrows)
 
 func _physics_process(delta):
 	var motion = Vector2(speed * delta, 0)
@@ -53,14 +57,14 @@ func _physics_process(delta):
 	var leftBoundary = barBackground.position.x - barHalfWidth + arrowHalfWidth
 
 	if arrow.position.x > rightBoundary:
-		speed = -abs(rand_range(200, 200))
+		speed = -abs(rand_range(200, 600))
 		if clicou:
 			cliques_restantes -= 1
 			print("Cliques restantes: ", cliques_restantes)
 		clicou = true
 		
 	elif arrow.position.x < leftBoundary:
-		speed = abs(rand_range(200, 200))
+		speed = abs(rand_range(200, 600))
 		if clicou:
 			cliques_restantes -= 1
 			print("Cliques restantes: ", cliques_restantes)
@@ -69,6 +73,7 @@ func _physics_process(delta):
 	var spotPosition = $GreenSpot.position.x
 	
 	if Input.is_action_just_pressed("ui_accept"):
+		arrowPlayer.sprite.visible = false
 		if clicou:
 			if arrow.position.x > greenSpot.position.x - 20 and arrow.position.x < greenSpot.position.x + 20:
 				total_de_acertos += 1
@@ -76,7 +81,7 @@ func _physics_process(delta):
 				clicou = false
 				for bot in bots:
 					bot.animation.play("avanca")
-				corda.position.x -= 15
+				animacaoTime2ParaTras()
 				print("Acertou: ", total_de_acertos)
 				print("Cliques restantes: ", cliques_restantes)
 			else:
@@ -84,31 +89,73 @@ func _physics_process(delta):
 				clicou = false
 				for bot in bots:
 					bot.animation.play("tras")
-				corda.position.x += 15		
+				animacaoTime1ParaTras()
 				print("Errou")
 				print("Cliques restantes: ", cliques_restantes)
 	
 	if cliques_restantes + total_de_acertos < 5:
-		corda.position.x += 200
+		for bot in bots:
+			bot.animation.stop()
+
 		if delay_timer < delay_duration:
 			delay_timer += delta
+			animacaoVitoria()
 		else:
 			get_tree().change_scene("res://scenes/GameOverScreen.tscn")
-			# Realize a operação que deseja executar após o atraso
-			# ...
-			# Redefina o temporizador de atraso
 			delay_timer = 0
-		
-		
 		
 	if total_de_acertos >= 5:
-		corda.position.x -= 200
+		for bot in bots:
+			bot.animation.stop()
+			
 		if delay_timer < delay_duration:
 			delay_timer += delta
+			animacaoPerda()
 		else:
 			get_tree().change_scene("res://scenes/WinScreen.tscn")
-			# Realize a operação que deseja executar após o atraso
-			# ...
-			# Redefina o temporizador de atraso
 			delay_timer = 0
+
+func animacaoTime1ParaTras():
+	corda.position.x += 15
+	bot3.position.x -= 5
+	bot2.position.x -= 5
+	bot1.position.x -= 5
+
+func animacaoTime2ParaTras():
+	corda.position.x -= 15
+	bot6.position.x += 5
+	bot5.position.x += 5
+	bot4.position.x += 5
+
+func animacaoPerda():
+	if delay_timer > 0.2:
+		corda.position.x -= 200
+	if delay_timer > 0.3:
+		bot6.position.x = 368
+	if delay_timer > 0.4:
+		bot6.position.y += 10
+	if delay_timer > 0.5:
+		bot5.position.x = 368
+	if delay_timer > 0.6:
+		bot5.position.y += 10
+	if delay_timer > 0.7:
+		bot4.position.x = 368
+	if delay_timer > 0.8:
+		bot4.position.y += 10
+		
+func animacaoVitoria():
+	if delay_timer > 0.2:
+		corda.position.x += 200
+	if delay_timer > 0.3:
+		bot3.position.x = -416
+	if delay_timer > 0.4:
+		bot3.position.y += 10
+	if delay_timer > 0.5:
+		bot2.position.x = -416
+	if delay_timer > 0.6:
+		bot2.position.y += 10
+	if delay_timer > 0.7:
+		bot1.position.x = -416
+	if delay_timer > 0.8:
+		bot1.position.y += 10
 		
